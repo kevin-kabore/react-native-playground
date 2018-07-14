21664773af4923581cd63daf400a1ad14ecf1471
Kevin Kabore <kevin.s.kabore@gmail.com>
2018-07-13 22:25:23 -0700

    creates AlbumList component

--- albums/index.js
+++ albums/index.js
@@ -2,10 +2,16 @@
 
 // import a library to help create a component
 import React from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, View } from 'react-native';
 import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 // Create a component
-const App = () => <Header />;
+const App = () => (
+  <View>
+    <Header headerText={'Albums'} />
+    <AlbumList />
+  </View>
+);
 
 // Render it to the device
 AppRegistry.registerComponent('albums', () => App);
