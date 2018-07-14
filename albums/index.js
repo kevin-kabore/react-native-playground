0dffe74e3f96fcd761a16c0a9f77a5e574ac3402
Kevin Kabore <kevin.s.kabore@gmail.com>
2018-07-13 22:50:16 -0700

    adds fetch ajax request  in comonentWIllMount

--- albums/index.js
+++ albums/index.js
@@ -1,17 +1,27 @@
 // index.ios.js - code for ios
 
 // import a library to help create a component
-import React from 'react';
+import React, { Component } from 'react';
 import { AppRegistry, View } from 'react-native';
 import Header from './src/components/Header';
 import AlbumList from './src/components/AlbumList';
+
 // Create a component
-const App = () => (
-  <View>
-    <Header headerText={'Albums'} />
-    <AlbumList />
-  </View>
-);
+class App extends Component {
+  componentWillMount() {
+    fetch('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(res => res.json())
+      .then(data => console.log(data));
+  }
+  render() {
+    return (
+      <View>
+        <Header headerText={'Albums'} />
+        <AlbumList />
+      </View>
+    );
+  }
+}
 
 // Render it to the device
 AppRegistry.registerComponent('albums', () => App);
