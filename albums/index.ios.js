db83892873b2c8daaa7ec5927b7ee2cbb5b6d877
Kevin Kabore <kevin.s.kabore@gmail.com>
2018-07-13 21:34:17 -0700

    Creates first react-native app and renders Header component

---
+++ albums/index.ios.js
@@ -0,0 +1,11 @@
+// // index.ios.js - code for ios
+//
+// // import a library to help create a component
+// import React from 'react';
+// import { AppRegistry, Text } from 'react-native';
+//
+// // Create a component
+// const App = () => <Text>Some Text</Text>;
+//
+// // Render it to the device
+// AppRegistry.registerComponent('albums', () => App);
---
+++ albums/index.js
@@ -0,0 +1,11 @@
+// index.ios.js - code for ios
+
+// import a library to help create a component
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import Header from './src/components/Header';
+// Create a component
+const App = () => <Header />;
+
+// Render it to the device
+AppRegistry.registerComponent('albums', () => App);
