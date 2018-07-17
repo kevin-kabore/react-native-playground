eef1238215035637abd60df8a7c30e3b5215023e
Kevin Kabore <kevin.s.kabore@gmail.com>
2018-07-16 23:14:42 -0700

    adds more styling for layout

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -4,24 +4,47 @@ import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
-const AlbumDetail = ({ album }) => (
-  <Card>
-    <CardSection>
-      <View>
-        <Image />
-      </View>
-      <View style={styles.textContainerStyle}>
-        <Text>{album.title}</Text>
-        <Text>{album.artist}</Text>
-      </View>
-    </CardSection>
-  </Card>
-);
+const AlbumDetail = ({ album }) => {
+  const { title, artist, thumbnail_image } = album;
+  const {
+    thumbnailStyle,
+    headerTextContainerStyle,
+    thumbnailContainerStyle,
+    headerTextStyle
+  } = styles;
+
+  return (
+    <Card>
+      <CardSection>
+        <View style={thumbnailContainerStyle}>
+          <Image style={thumbnailStyle} source={{ uri: thumbnail_image }} />
+        </View>
+        <View style={headerTextContainerStyle}>
+          <Text style={headerTextStyle}>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
+      </CardSection>
+    </Card>
+  );
+};
 
 const styles = {
-  textContainerStyle: {
+  headerTextContainerStyle: {
     flexDirection: 'column',
     justifyContent: 'space-around'
+  },
+  headerTextStyle: {
+    fontSize: 18
+  },
+  thumbnailStyle: {
+    width: 50,
+    height: 50
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
   }
 };
 
