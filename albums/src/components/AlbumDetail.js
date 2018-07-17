677cfaf711aacf75f94ad9686d40a099259def8f
Kevin Kabore <kevin.s.kabore@gmail.com>
2018-07-17 00:30:59 -0700

    Completes component

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -26,9 +26,11 @@ const AlbumDetail = ({ album }) => {
           <Text>{artist}</Text>
         </View>
       </CardSection>
+
       <CardSection>
         <Image style={imageStyle} source={{ uri: image }} />
       </CardSection>
+
       <CardSection>
         <Button />
       </CardSection>
