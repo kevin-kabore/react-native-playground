ce95b0fbf19080f155f08515bdf87d36ab2ab9cb
Kevin Kabore <kevin.s.kabore@gmail.com>
2018-07-25 15:24:59 -0700

    adds firebase support

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
-
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
 class App extends Component {
+  componentWIllMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyDWbi76u_9HM6FOWz-bchBqH9KHPFHcqVA',
+      authDomain: 'authentication-509db.firebaseapp.com',
+      databaseURL: 'https://authentication-509db.firebaseio.com',
+      projectId: 'authentication-509db',
+      storageBucket: 'authentication-509db.appspot.com',
+      messagingSenderId: '973307232062'
+    });
+  }
   render() {
     return (
       <View>
